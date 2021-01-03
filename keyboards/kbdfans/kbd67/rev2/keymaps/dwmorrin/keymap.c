0bd4175dc83c941025a7ff2f66797bd7c625b4f8
David Morrin <dwmorrin@gmail.com>
2021-01-03 10:20:02 -0500

    feat: changes to split space

--- keyboards/kbdfans/kbd67/rev2/keymaps/dwmorrin/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/dwmorrin/keymap.c
@@ -47,15 +47,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|TG(1) |PgU|F4  |
    * |----------------------------------------------------------------|
-   * |Ctrl|Alt |Cmd |        Space          |Cmd|Alt|Ctrl|F8 |PgD|F12 |
+   * |Ctrl|Alt |Cmd | Command| TG(1)| Space |Cmd|Alt|Ctrl|F8 |PgD|F12 |
    * `----------------------------------------------------------------'
    */
-[0] = LAYOUT_65_ansi(
+[0] = LAYOUT_65_ansi_split_space(
   KC_GRV,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_F1, \
   KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_F2, \
   KC_ESC,  KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_F3,  \
   KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, TG(1),            KC_PGUP, KC_F4, \
-  KC_LCTL, KC_LALT, KC_LGUI,                KC_SPC,                          KC_RGUI, KC_RALT, KC_RCTL, KC_F8,   KC_PGDN, KC_F12),
+  KC_LCTL, KC_LALT, KC_LGUI,KC_LGUI,TG(1),  KC_SPC,                          KC_RGUI, KC_RALT, KC_RCTL, KC_F8,   KC_PGDN, KC_F12),
 
   /* Keymap Vi Layer: pseudo vi normal mode
    * ,----------------------------------------------------------------.
@@ -70,12 +70,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |                       |   |   |    |   |   |    |
    * `----------------------------------------------------------------'
    */
-[1] = LAYOUT_65_ansi(
+[1] = LAYOUT_65_ansi_split_space(
   _______, _______, _______, _______,  DM_END, _______, _______, _______, _______, _______,  DM_HOME, _______,  _______, KC_LEFT,  _______, \
   _______, _______, DM_WORD, _______, _______, _______, _______, _______, TO(0),   _______,  _______, _______, _______, _______, _______, \
   _______, TO(0),   _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,          _______, _______, \
   _______, _______, KC_DEL,  _______, _______, DM_BACK, _______, _______, _______, _______,  _______, _______,          _______, _______, \
-  _______, _______, _______,                   _______,                            _______,  _______, _______, _______, _______, _______),
+  _______, _______, _______, _______, _______, _______,                            _______,  _______, _______, _______, _______, _______),
 
 };
 
